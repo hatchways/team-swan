906478e834fd27484d98288b6099b92c3ec39742
Ritesh <catalight.github.io>
2020-05-16 19:18:30 -0400

    Forgot to add the updated model

--- server/models/user.js
+++ server/models/user.js
@@ -1,10 +1,67 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    email: DataTypes.STRING
-  }, {});
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER
+    },
+    firstName: {
+      type: Sequelize.STRING,
+        allowNull: false,   
+        validate: {
+            notEmpty: {
+                msg: '"First Name" is required'
+            },
+        },   
+    },
+    lastName: {
+      type: Sequelize.STRING,
+      allowNull: false,  
+      validate: {
+          notEmpty: {
+              msg: '"Last Name" is required'
+          },
+      },    
+    },
+    email: {
+      type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: {
+                msg: '"Email Address" is required'
+            },
+            isEmail: {
+                msg: '"Email Address" is not valid'
+            }, 
+        }, 
+        unique: {
+            args: true,
+            msg: 'Email address already in use!'
+        },
+    },
+    password: {
+      type: Sequelize.STRING,
+      allowNull: false,   
+      validate: {
+          notEmpty: {
+              msg: '"Password" is required'
+          },
+      },   
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    }
+ }, {
+  sequelize,
+  modelName: 'Course'
+ });
   User.associate = function(models) {
     // associations can be defined here
   };
