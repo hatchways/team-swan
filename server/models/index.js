099172db8ef6cc38d4e8807a4a3ede9a988a9408
Ritesh Maharjan <DimWitRM@gmail.com>
2020-05-19 14:59:34 -0400

    Update index.js

--- server/models/index.js
+++ server/models/index.js
@@ -8,7 +8,7 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
-let sequelize = new Sequelize(config.database, config.username, config.password, config);
+const sequelize = new Sequelize(config.database, config.username, config.password, config);
 
 try{
   // adding table/model in the database
