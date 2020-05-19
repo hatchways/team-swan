8a4f4db658d983a7987948006f63e82081f7739b
Ritesh <catalight.github.io>
2020-05-19 12:18:58 -0400

    Fixed the mentioned reviews

--- server/config/config.js
+++ server/config/config.js
@@ -15,20 +15,4 @@ module.exports = {
       idle: 10000
     }
   }
-  // test: {
-  //   username: root,
-  //   password: null,
-  //   database: database_test,
-  //   host: "127.0.0.1",
-  //   dialect: postgres,
-  //   operatorsAliases: false
-  // },
-  // production: {
-  //   username: root,
-  //   password: null,
-  //   database: database_production,
-  //   host: "127.0.0.1",
-  //   dialect: postgres,
-  //   operatorsAliases: false
-  // }
 }
--- server/models/user.js
+++ server/models/user.js
@@ -60,7 +60,7 @@ module.exports = (sequelize, DataTypes) => {
     }
  }, {
   sequelize,
-  modelName: 'Course'
+  modelName: 'User'
  });
   User.associate = function(models) {
     // associations can be defined here
