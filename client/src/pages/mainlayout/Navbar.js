f3f34b83ce5051115cde2d536114a91feb8a05b9
Christian <43688000+christianromarserad@users.noreply.github.com>
2020-05-20 15:31:40 -0400

    Updated Navbar component

--- client/src/pages/mainlayout/Navbar.js
+++ client/src/pages/mainlayout/Navbar.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { AppBar, Toolbar, makeStyles } from "@material-ui/core";
 import logo from "./logo.png";
+import withAuth from "common/withAuth";
+import Navlinks from "./Navlinks";
+import UserInfo from "./UserInfo";
 
 //CSS styles
 const useStyles = makeStyles({
@@ -12,17 +15,20 @@ const useStyles = makeStyles({
   }
 });
 
-const Navbar = () => {
+const Navbar = ({ isAuthenticated, user, logout }) => {
   const { toolbar, imageContainer } = useStyles();
+
   return (
     <AppBar color="secondary" elevation={0} position="relative">
       <Toolbar className={toolbar}>
         <div className={imageContainer}>
           <img src={logo} width="130" />
         </div>
+        <Navlinks />
+        <UserInfo />
       </Toolbar>
     </AppBar>
   );
 };
 
-export default Navbar;
+export default withAuth(Navbar, false);
