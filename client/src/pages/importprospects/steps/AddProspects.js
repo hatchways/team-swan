9e7b88b41b27f61af5513e7f3445c205a0db4c88
shah62 <shah62@users.noreply.github.com>
2020-05-26 03:39:36 -0400

    Created import prospect page

---
+++ client/src/pages/importprospects/steps/AddProspects.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Button } from '@material-ui/core';
+import useSnackbar from 'common/useSnackbar'
+import axios from 'axios';
+
+export default ({ mappedAttributes, setActiveStep }) => {
+
+    const showSnackbar = useSnackbar()
+
+    const onclick = async () => {
+        try {
+            const resp = await axios.post('/api/addprospects', { mappedAttributes })
+            showSnackbar(resp.data.message, 'success')
+            setActiveStep((prevStep) => prevStep + 1)
+        } catch (err) {
+            showSnackbar(
+                err.response && err.response.data.errors ? err.response.data.errors[0].message : "Error!",
+                "error"
+            );
+        }
+    }
+
+    return (
+        <Button onClick={onclick}>
+            Add Prospects
+        </Button>
+    )
+}
---
+++ client/src/pages/importprospects/steps/MapCSV.js
@@ -0,0 +1,87 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { withStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import { NativeSelect } from "@material-ui/core";
+
+const StyledTableCell = withStyles((theme) => ({
+  head: {
+    backgroundColor: theme.palette.success.main,
+    textAlign: "center;",
+    color: theme.palette.common.white,
+  },
+  body: {
+    fontSize: 14,
+  },
+}))(TableCell);
+
+const StyledTableRow = withStyles((theme) => ({
+  root: {
+    // '&:nth-of-type(odd)': {
+    //   backgroundColor: theme.palette.action.hover,
+    // },
+  },
+}))(TableRow);
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 700,
+  },
+});
+
+export default function MapCSV({ tableHead, attributes, properties, mappedAttributes, setMappedAttributes }) {
+  const classes = useStyles();
+  const [propertiesAvailable, setPropertiesAvailable] = React.useState(properties)
+
+
+  const onChange = (event) => {
+    const dbValue = event.target.value
+    const attrFromCSV = event.target.name
+    setPropertiesAvailable(propertiesAvailable.filter(attr => attr !== dbValue))
+    setMappedAttributes({ ...mappedAttributes, [attrFromCSV]: dbValue })
+  }
+
+  return (
+    <TableContainer component={Paper}>
+      <Table className={classes.table} aria-label="customized table">
+        <TableHead>
+          <TableRow>
+            {tableHead.map((element) => {
+              return (
+                <StyledTableCell key={element}>{element}</StyledTableCell>
+              )
+            })}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {attributes.map((attr) => {
+            return (
+              <StyledTableRow key={Math.random()}>
+                <StyledTableCell align="center" component="th" scope="row">
+                  {attr}
+                </StyledTableCell>
+                <StyledTableCell align="center">
+                  <NativeSelect name={attr} value={mappedAttributes[attr]} onChange={onChange}>
+                    <option value="">None</option>
+                    {mappedAttributes[attr] &&
+                      <option value={mappedAttributes[attr]}>
+                        {mappedAttributes[attr]}
+                      </option>}
+                    {properties.map((property) => {
+                      return (<option value={property} key={property}>{property}</option>)
+                    })}
+                  </NativeSelect>
+                </StyledTableCell>
+              </StyledTableRow>)
+          })}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
