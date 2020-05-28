fa582b79039eaddd1c024f8d4db77d7d40619599
Christian <43688000+christianromarserad@users.noreply.github.com>
2020-05-28 19:28:10 -0400

    Add individual campaigns route

--- client/src/App.js
+++ client/src/App.js
@@ -5,16 +5,24 @@ import Login from "./pages/login";
 import Signup from "./pages/signup";
 import withAuth from "common/withAuth";
 import Campaigns from "./pages/campaigns";
+import Campaign from "./pages/campaign";
 import Prospects from "./pages/prospects";
 import Templates from "./pages/templates";
 import Reporting from "./pages/reporting";
-import { campaigns, prospects, templates, reporting } from "constants/routes";
+import {
+  campaigns,
+  campaign,
+  prospects,
+  templates,
+  reporting
+} from "constants/routes";
 
 function App({ isAuthenticated }) {
   return (
     <MainLayout>
       <Switch>
         <Route path={prospects} component={Prospects} />
+        <Route path={campaign} component={Campaign} />
         <Route path={campaigns} component={Campaigns} />
         <Route path={reporting} component={Reporting} />
         <Route path={templates} component={Templates} />
