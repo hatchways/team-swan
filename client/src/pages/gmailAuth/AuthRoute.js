fe8ad4ecb954917a0cca9e53c8915b5e8731320c
shah62 <shah62@users.noreply.github.com>
2020-05-29 11:19:43 -0400

    Upadted file for handling gmail authentication

--- client/src/pages/gmailAuth/AuthRoute.js
+++ client/src/pages/gmailAuth/AuthRoute.js
@@ -1,37 +1,31 @@
-import React from 'react'
-import axios from 'axios'
-import queryString from 'query-string'
+import React from "react";
+import axios from "axios";
+import queryString from "query-string";
 import withAuth from "common/withAuth";
-import useSnackbar from 'common/useSnackbar';
+import useSnackbar from "common/useSnackbar";
 
 // A Route that responds when OAuth redirects the user back to our application
 
 const Auth = (props) => {
+  const showSnackBar = useSnackbar();
 
-    const showSnackBar = useSnackbar()
-
-    React.useEffect(() => {
-        const checkAuth = async () => {
-            try {
-                const value = queryString.parse(props.location.search)
-                const res = await axios.post('/gmail/token', { code: value.code })
-                if (res.data.message) {
-                    showSnackBar('Authorization Success')
-                    props.history.push("/")
-                }
-            } catch (error) {
-                showSnackBar('Failed to Authenticate')
-            }
+  React.useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        const value = queryString.parse(props.location.search);
+        const res = await axios.post("/gmail/token", { code: value.code });
+        if (res.data.message) {
+          showSnackBar("Authorization Success");
+          props.history.push("/");
         }
-        checkAuth()
-    }, [props.history, props.location.search, showSnackBar])
+      } catch (error) {
+        showSnackBar("Failed to Authenticate");
+      }
+    };
+    checkAuth();
+  }, [props.history, props.location.search, showSnackBar]);
 
-    return (
-        <div>
-            {props.history.push("/")}
-        </div>
-    )
+  return <div>{props.history.push("/")}</div>;
+};
 
-}
-
-export default withAuth(Auth)
+export default Auth;
