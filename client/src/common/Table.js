cacbdd4a9b5558044a7e585fdef335e56bad6f47
Christian <43688000+christianromarserad@users.noreply.github.com>
2020-06-03 19:46:02 -0400

    Added a table

--- client/src/common/Table.js
+++ client/src/common/Table.js
@@ -1,143 +1,56 @@
-import React from "react";
-import { withStyles, makeStyles } from "@material-ui/core/styles";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableContainer from "@material-ui/core/TableContainer";
-import TableHead from "@material-ui/core/TableHead";
-import TableRow from "@material-ui/core/TableRow";
-import Paper from "@material-ui/core/Paper";
-import PropTypes from "prop-types";
-import { useTheme } from "@material-ui/core/styles";
-import TableFooter from "@material-ui/core/TableFooter";
-import TablePagination from "@material-ui/core/TablePagination";
-import IconButton from "@material-ui/core/IconButton";
-import FirstPageIcon from "@material-ui/icons/FirstPage";
-import KeyboardArrowLeft from "@material-ui/icons/KeyboardArrowLeft";
-import KeyboardArrowRight from "@material-ui/icons/KeyboardArrowRight";
-import LastPageIcon from "@material-ui/icons/LastPage";
+import React, { useState, useEffect } from "react";
+import {
+  makeStyles,
+  Paper,
+  Table as MaterialTable,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+  TablePagination,
+} from "@material-ui/core";
+import { toSentenceCase } from "js-convert-case";
 
-import { SearchContext } from "../index";
-
-const StyledTableCell = withStyles((theme) => ({
-  head: {
-    backgroundColor: "#4fbe75",
-    color: theme.palette.common.white,
+const useStyles = makeStyles((theme) => ({
+  tableContainer: {
+    padding: "1rem",
+    "& td, th": {
+      fontWeight: "bold",
+    },
+    "& td": {
+      borderBottom: `1px solid ${theme.palette.grey[200]}`,
+    },
   },
-  body: {
-    fontSize: 14,
-  },
-}))(TableCell);
-
-const useStyles1 = makeStyles((theme) => ({
-  root: {
-    flexShrink: 0,
-    marginLeft: theme.spacing(2.5),
+  tableHead: {
+    backgroundColor: theme.palette.primary.main,
+    "& th": {
+      color: "#ffffff",
+      border: "none",
+    },
+    "& th:first-child": {
+      borderRadius: "5px 0 0 5px",
+    },
+    "& th:last-child, ": {
+      borderRadius: "0  5px 5px 0",
+    },
   },
 }));
 
-function TablePaginationActions(props) {
-  const classes = useStyles1();
-  const theme = useTheme();
-  const { count, page, rowsPerPage, onChangePage } = props;
+const Table = ({ data, renderRowHeader, renderRowData }) => {
+  const [paginatedData, setPaginatedData] = useState([]);
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
 
-  const handleFirstPageButtonClick = (event) => {
-    onChangePage(event, 0);
-  };
+  const { tableContainer, tableHead } = useStyles();
 
-  const handleBackButtonClick = (event) => {
-    onChangePage(event, page - 1);
-  };
-
-  const handleNextButtonClick = (event) => {
-    onChangePage(event, page + 1);
-  };
-
-  const handleLastPageButtonClick = (event) => {
-    onChangePage(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
-  };
-
-  return (
-    <div className={classes.root}>
-      <IconButton
-        onClick={handleFirstPageButtonClick}
-        disabled={page === 0}
-        aria-label="first page"
-      >
-        {theme.direction === "rtl" ? <LastPageIcon /> : <FirstPageIcon />}
-      </IconButton>
-      <IconButton
-        onClick={handleBackButtonClick}
-        disabled={page === 0}
-        aria-label="previous page"
-      >
-        {theme.direction === "rtl" ? (
-          <KeyboardArrowRight />
-        ) : (
-          <KeyboardArrowLeft />
-        )}
-      </IconButton>
-      <IconButton
-        onClick={handleNextButtonClick}
-        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-        aria-label="next page"
-      >
-        {theme.direction === "rtl" ? (
-          <KeyboardArrowLeft />
-        ) : (
-          <KeyboardArrowRight />
-        )}
-      </IconButton>
-      <IconButton
-        onClick={handleLastPageButtonClick}
-        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-        aria-label="last page"
-      >
-        {theme.direction === "rtl" ? <FirstPageIcon /> : <LastPageIcon />}
-      </IconButton>
-    </div>
-  );
-}
-
-TablePaginationActions.propTypes = {
-  count: PropTypes.number.isRequired,
-  onChangePage: PropTypes.func.isRequired,
-  page: PropTypes.number.isRequired,
-  rowsPerPage: PropTypes.number.isRequired,
-};
-
-const useStyles2 = makeStyles({
-  table: {
-    minWidth: 500,
-  },
-  tableContainer: {
-    padding: "0.5rem",
-  },
-});
-
-const rowData = ["Id", "Email", "Status", "Firstname", "Lastname"];
-const tableBodyData = ["email", "status", "firstName", "lastName"];
-
-export default function CustomPaginationActionsTable(props) {
-  const classes = useStyles2();
-  const [page, setPage] = React.useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(10);
-
-  const search = React.useContext(SearchContext).state.inputText;
-
-  let rows = props.prospectData;
-  const [filteredData, setFilteredData] = React.useState(rows);
-  React.useEffect(() => {
-    if (props.prospectData.length !== 0) {
-      const newData = props.prospectData.filter((prospect) => {
-        return prospect.email.includes(search);
-      });
-      setFilteredData(newData);
-    }
-  }, [props.prospectData, search]);
-
-  const emptyRows =
-    rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
+  //Paginate the data
+  useEffect(() => {
+    console.log(page);
+    const startPosition = page * rowsPerPage;
+    const endPosition = page * rowsPerPage + rowsPerPage;
+    const paginatedData = data.slice(startPosition, endPosition);
+    setPaginatedData(paginatedData);
+  }, [page, rowsPerPage, data]);
 
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
@@ -148,61 +61,68 @@ export default function CustomPaginationActionsTable(props) {
     setPage(0);
   };
 
-  return (
-    <TableContainer component={Paper} className={classes.tableContainer}>
-      <Table className={classes.table} aria-label="custom pagination table">
-        <TableHead>
-          <TableRow>
-            {rowData.map((rowHead) => {
-              return <StyledTableCell>{rowHead}</StyledTableCell>;
-            })}
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {(rowsPerPage > 0
-            ? filteredData.slice(
-                page * rowsPerPage,
-                page * rowsPerPage + rowsPerPage
-              )
-            : filteredData
-          ).map((row, index) => (
-            <TableRow key={row.id}>
-              <TableCell>{index + 1}</TableCell>
-              {tableBodyData.map((tableBodyData) => {
-                return (
-                  <TableCell key={row[tableBodyData]}>
-                    {row[tableBodyData]}
-                  </TableCell>
-                );
-              })}
-            </TableRow>
-          ))}
+  const renderHeader = () => {
+    if (renderRowHeader) {
+      return renderRowHeader(data);
+    } else {
+      return (
+        <TableRow>
+          {Object.keys(data[0]).map((columnName) => {
+            if (columnName !== "id") {
+              return (
+                <TableCell key={columnName}>
+                  {toSentenceCase(columnName)}
+                </TableCell>
+              );
+            }
+          })}
+        </TableRow>
+      );
+    }
+  };
 
-          {emptyRows > 0 && (
-            <TableRow style={{ height: 53 * emptyRows }}>
-              <TableCell colSpan={6} />
-            </TableRow>
-          )}
-        </TableBody>
-        <TableFooter>
-          <TableRow>
-            <TablePagination
-              rowsPerPageOptions={[5, 10, 25, { label: "All", value: -1 }]}
-              colSpan={3}
-              count={rows.length}
-              rowsPerPage={rowsPerPage}
-              page={page}
-              SelectProps={{
-                inputProps: { "aria-label": "rows per page" },
-                native: true,
-              }}
-              onChangePage={handleChangePage}
-              onChangeRowsPerPage={handleChangeRowsPerPage}
-              ActionsComponent={TablePaginationActions}
-            />
+  const renderRow = () => {
+    return paginatedData.map((dataObject) => {
+      if (renderRowData) {
+        return renderRowData(dataObject);
+      } else {
+        return (
+          <TableRow key={dataObject}>
+            {Object.keys(dataObject).map(
+              (columnName) =>
+                columnName !== "id" && (
+                  <TableCell>{dataObject[columnName]}</TableCell>
+                )
+            )}
           </TableRow>
-        </TableFooter>
-      </Table>
-    </TableContainer>
+        );
+      }
+    });
+  };
+
+  return (
+    <React.Fragment>
+      {data.length !== 0 ? (
+        <>
+          <TablePagination
+            rowsPerPageOptions={[5, 10, 25, { label: "All", value: -1 }]}
+            component="div"
+            count={data.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onChangePage={handleChangePage}
+            onChangeRowsPerPage={handleChangeRowsPerPage}
+          />
+          <Paper className={tableContainer} component={Paper}>
+            <MaterialTable>
+              <TableHead className={tableHead}>{renderHeader()}</TableHead>
+              <TableBody>{renderRow()}</TableBody>
+            </MaterialTable>
+          </Paper>
+        </>
+      ) : null}
+    </React.Fragment>
   );
-}
+};
+
+export default Table;
