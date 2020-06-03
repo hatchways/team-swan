ccb8c3e717ee7e89fc5ac78fb859fcc4a8a1ffd7
Christian <43688000+christianromarserad@users.noreply.github.com>
2020-06-03 13:22:38 -0400

    Adding campaigns page file and table

--- client/src/pages/campaigns/index.js
+++ client/src/pages/campaigns/index.js
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import withAuth from "common/withAuth";
 import Drawer from "common/Drawer";
+import axios from "axios";
+import { campaign } from "constants/routes";
 
 //TODO campaigns page
 const Campaigns = () => {
-  return <h1>Campaigns page</h1>;
+  const [campaignsData, setCampaignsData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
+
+  useEffect(() => {
+    axios.get("/api/campaign").then((response) => {
+      setCampaignsData(response.data);
+    });
+  }, []);
+
+  useEffect(() => {
+    setFilteredData(campaignsData);
+  }, [campaignsData]);
+
+  return (
+    <Drawer>
+      <h1>Hello</h1>
+    </Drawer>
+  );
 };
 
 export default withAuth(Campaigns);
