b3b333c6b109012a9b45c27e5a991d52b947b877
Christian <43688000+christianromarserad@users.noreply.github.com>
2020-06-04 12:53:17 -0400

    Fix navbar bug

--- client/src/pages/mainlayout/Navbar.js
+++ client/src/pages/mainlayout/Navbar.js
@@ -19,28 +19,34 @@ const useStyles = makeStyles((theme) => ({
   imageContainer: {
     flexGrow: 1,
   },
+  navbarHeight: {
+    height: "6rem",
+  },
 }));
 
 const Navbar = ({ isAuthenticated, user, logout }) => {
-  const { navbar, imageContainer } = useStyles();
+  const { navbar, imageContainer, navbarHeight } = useStyles();
 
   return (
-    <AppBar
-      className={navbar}
-      color="secondary"
-      elevation={0}
-      position="relative"
-    >
-      <div className={imageContainer}>
-        <img src={logo} width="130" />
-      </div>
-      {isAuthenticated && (
-        <>
-          <Navlinks />
-          <Profile user={user} logout={logout} />
-        </>
-      )}
-    </AppBar>
+    <>
+      <AppBar
+        className={navbar}
+        color="secondary"
+        elevation={0}
+        position="fixed"
+      >
+        <div className={imageContainer}>
+          <img src={logo} width="130" />
+        </div>
+        {isAuthenticated && (
+          <>
+            <Navlinks />
+            <Profile user={user} logout={logout} />
+          </>
+        )}
+      </AppBar>
+      <div className={navbarHeight}></div>
+    </>
   );
 };
 
