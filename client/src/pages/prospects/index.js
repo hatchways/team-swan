019c401e045a71afe0ac64a81b11e35e1ee4318c
shah62 <shah62@users.noreply.github.com>
2020-06-08 13:17:12 -0400

    Updated prospect page

--- client/src/pages/prospects/index.js
+++ client/src/pages/prospects/index.js
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import withAuth from "common/withAuth";
 import Drawer from "common/Drawer";
-import CustomizedTable from "pages/prospects/component/Table";
 import axios from "axios";
 import { Container, Grid, Typography, Button } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import SearchSection from "pages/prospects/component/SearchSection";
+import useSnackbar from "common/useSnackbar";
+import SideBarContent from "./SideBarContent";
+import Content from "./Content";
 
 const useStyles = makeStyles((theme) => ({
   rootContainer: {
@@ -16,87 +17,55 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-// Create context object
-export const SearchContext = React.createContext();
-
-// Set up Initial State
-const initialState = {
-  inputText: "",
-};
-
-function reducer(state, action) {
-  switch (action.type) {
-    case "UPDATE_INPUT":
-      return {
-        inputText: action.data,
-      };
-
-    default:
-      return initialState;
-  }
-}
-
-//TODO propects page
 const Prospects = (props) => {
   const classes = useStyles();
   const id = props.user.id;
-  const [state, dispatch] = React.useReducer(reducer, initialState);
-  const prevId = React.useRef();
+  const [prospectsData, setProspectsData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
+  const [searchText, setSearchText] = useState("");
+  const [searchField, setSearchField] = useState("email");
+  const [isDataLoading, setIsDataLoading] = useState(true);
 
-  const [prospectData, setProspectData] = React.useState([]);
+  const showSnackbar = useSnackbar();
 
-  React.useEffect(() => {
-    const getProspects = async () => {
-      if (id) {
-        const prospects = await axios.get(`/api/user/${id}/prospects`);
-        setProspectData(prospects.data);
-      }
-    };
-    if (prevId.current !== id) {
-      getProspects();
+  useEffect(() => {
+    setIsDataLoading(true);
+    if (props.user.id) {
+      axios.get(`/api/user/${id}/prospects`).then((response) => {
+        setProspectsData(response.data);
+        setFilteredData(response.data);
+        setIsDataLoading(false);
+      });
     }
-  }, [id]);
+  }, [id, props.user.id]);
 
-  React.useEffect(() => {
-    prevId.current = id;
-  }, [id]);
-
-  const left = <SearchSection></SearchSection>;
-
-  const right = (
-    <div>
-      <Container className={classes.rootContainer}>
-        <div>
-          <Grid container spacing={3}>
-            <Grid item xs={9} className={classes.gridElement}>
-              <Typography variant="h4">Prospects</Typography>
-            </Grid>
-            <Grid className={classes.gridElement} justify={"flex-end"}>
-              <Button
-                variant="contained"
-                size="large"
-                color="primary"
-                onClick={() => props.history.push("/prospects/import")}
-              >
-                Import Prospects
-              </Button>
-            </Grid>
-          </Grid>
-        </div>
-      </Container>
-      <CustomizedTable prospectData={prospectData}></CustomizedTable>
-    </div>
-  );
+  useEffect(() => {
+    const newFilteredData = prospectsData.filter((data) =>
+      data[searchField].toLowerCase().includes(searchText.toLowerCase())
+    );
+    setFilteredData(newFilteredData);
+  }, [prospectsData, searchField, searchText]);
 
   return (
-    <SearchContext.Provider value={{ state, dispatch }}>
-      <div>
-        <Drawer
-          LeftDrawerComponent={left}
-          RightDrawerComponent={right}
-        ></Drawer>
-      </div>
-    </SearchContext.Provider>
+    <>
+      <Drawer
+        LeftDrawerComponent={
+          <SideBarContent
+            searchProspectText={searchText}
+            setSearchProspectText={setSearchText}
+            searchField={searchField}
+            setSearchField={setSearchField}
+          />
+        }
+        RightDrawerComponent={
+          <Content
+            isDataLoading={isDataLoading}
+            history={props.history}
+            filteredData={filteredData}
+          />
+        }
+      />
+    </>
   );
 };
 
