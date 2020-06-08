218959f571213f353d5b382e189f619b93438e41
shah62 <shah62@users.noreply.github.com>
2020-06-08 13:18:36 -0400

    Rename the gmail router filename

---
+++ server/src/routes/gmail.js
@@ -0,0 +1,13 @@
+// A router that handles all gmail auth routes
+const router = require("express").Router();
+const Gmail = require("../controllers/gmail");
+const requireAuth = require("../middleware/require-auth");
+const { body } = require("express-validator");
+const validateRequest = require("../middleware/validate-request");
+
+router.get("/gmail/authurl", requireAuth, Gmail.getAuthURL);
+router.post("/gmail/token", requireAuth, Gmail.generateToken);
+// This is a temporary route future requests should directly use the controller rather than this route
+router.post("/gmail/sendEmails", requireAuth, Gmail.tempRouteSendEmail);
+
+module.exports = router;
