e2bd1650589fab7e9d510a59405c900c16ff2fd3
shah62 <shah62@users.noreply.github.com>
2020-06-11 19:42:44 -0400

    Added socket support on email sending

--- server/src/controllers/gmail.js
+++ server/src/controllers/gmail.js
@@ -3,6 +3,8 @@ const DataBaseConnectionError = require("../errors/database-connection-error");
 const db = require("../../database/models/index");
 const EmailQueue = require("../utils/queue");
 const Mail = require("../utils/mail");
+const Socket = require("../utils/socketmanager");
+const Cache = require("../utils/cache");
 
 // The controller for Gmail Client
 class Gmail {
@@ -23,7 +25,7 @@ class Gmail {
 
   // Responds to route that gets the Authroriztion URL from google
   static getAuthURL = async (req, res) => {
-    const SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"];
+    const SCOPES = ["https://mail.google.com"];
 
     try {
       const authUrl = this.get_oAuthClient().generateAuthUrl({
@@ -62,16 +64,9 @@ class Gmail {
     });
   };
 
-  static getAuthorizedClient = async (userId) => {
+  static getAuthorizedClient = async (token) => {
     try {
       const auth = this.get_oAuthClient();
-      const token = await db.Token.findOne({
-        where: {
-          userId: userId,
-        },
-      });
-      if (!token) throw new Error("No token found!");
-
       auth.setCredentials({
         access_token: token.access_token,
         refresh_token: token.refresh_token,
@@ -87,8 +82,25 @@ class Gmail {
     }
   };
 
+  static getToken = async (id) => {
+    try {
+      const token = await db.Token.findOne({
+        where: {
+          userId: id,
+        },
+      });
+      if (!token) throw new Error("No token found!");
+      const returnToken = JSON.parse(JSON.stringify(token));
+      return returnToken;
+    } catch (error) {
+      console.log("Error authorizing Gmail Client\n", error);
+      Promise.reject(error.message);
+    }
+  };
+
   static getGmailAddress = async (id) => {
-    const gmail = await this.getAuthorizedClient(id);
+    const token = await this.getToken(id);
+    const gmail = await this.getAuthorizedClient(token);
     const gmailUserClient = gmail.users;
     const { emailAddress } = (
       await gmailUserClient.getProfile({
@@ -99,59 +111,145 @@ class Gmail {
     return emailAddress;
   };
 
-  static sendEmail = async (email, userId) => {
+  static sendEmail = async (email, token) => {
     try {
-      const gmail = await this.getAuthorizedClient(userId);
-      gmail.users.messages.send({
+      const gmail = await this.getAuthorizedClient(token);
+      const sent = gmail.users.messages.send({
         userId: "me",
         requestBody: { raw: email },
       });
+      return sent;
     } catch (err) {
       console.log(err);
       Promise.reject(err.message);
     }
   };
 
-  static addToQueue = async (encodedEmails, userId) => {
+  static addToQueue = async (encodedEmails, userId, stepProspectIds) => {
     let time = 5000;
     const addTime = 5000;
-    encodedEmails.forEach((endodedEmail) => {
-      const data = { endodedEmail, userId };
+    const token = await this.getToken(userId);
+    encodedEmails.forEach(async (endodedEmail, index) => {
+      const stepProspectId = stepProspectIds[index];
+      const data = { endodedEmail, token, stepProspectId, userId };
       const options = {
         delay: time,
+        attempts: 1,
       };
       time = time + addTime;
-      EmailQueue.add(data, options);
+      await EmailQueue.add(data, options);
     });
   };
 
-  static parseEmails = async (emails, userName, userId, body, subject) => {
+  static createEmailsAndAddToQueue = async (
+    emails,
+    userName,
+    userId,
+    body,
+    subject,
+    stepProspectIds
+  ) => {
     const gmailAddress = await this.getGmailAddress(userId);
     const from = `${userName} \u003c${gmailAddress}\u003e`;
-    const encodedEmails = emails.map((email) => {
-      const parsedEmail = Mail.parseMail(body);
-      const encodedEmail = Mail.encodeEmail(email, from, subject, parsedEmail);
-      return encodedEmail;
-    });
-    this.addToQueue(encodedEmails, userId);
+    if (Array.isArray(emails) && emails.length > 0) {
+      const encodedEmails = emails.map((email) => {
+        const parsedEmail = Mail.parseMail(body);
+        const encodedEmail = Mail.encodeEmail(
+          email,
+          from,
+          subject,
+          parsedEmail
+        );
+        return encodedEmail;
+      });
+      this.addToQueue(encodedEmails, userId, stepProspectIds);
+    }
   };
 
-  static tempRouteSendEmail = async (req, res) => {
-    const emails = req.body.emails;
-    const userName = "Donald Trump";
+  static sendEmailRoute = async (req, res) => {
+    console.log(`\n\n\n---------------\n${req.body}\n\n\n`);
+    const stepId = req.body.id;
+    const step = await db.Step.findOne({ where: { id: stepId } });
+    const stepProspects = await db.StepProspect.findAll({ where: { stepId } });
+    const campaignProspects = await Promise.all(
+      stepProspects.map(async (stepProspect) => {
+        return await db.CampaignProspect.findOne({
+          where: {
+            id: stepProspect.campaignProspectId,
+            // contacted: false,
+          },
+        });
+      })
+    );
+    const prospects = await Promise.all(
+      campaignProspects.map(async (campaignProspect) => {
+        return await db.Prospect.findOne({
+          where: { id: campaignProspect.prospectId },
+        });
+      })
+    );
+    const emails = prospects.map((prospect) => prospect.email);
+    const stepProspectIds = stepProspects.map(
+      (stepProspect) => stepProspect.stepId
+    );
+    const socketObj = {
+      open: true,
+      sent: 0,
+      total: emails.length,
+      newQueue: true,
+    };
+    Cache.setSocketObj(req.currentUser.id, socketObj);
+    console.log(emails);
+    const userName = `${req.currentUser.firstName} ${req.currentUser.lastName}`;
     const userId = req.currentUser.id;
-    const body = "This is a very important message";
-    const subject = "Hello world";
-    this.parseEmails(emails, userName, userId, body, subject);
+    const body = step.body;
+    const subject = step.subject;
+    this.createEmailsAndAddToQueue(
+      emails,
+      userName,
+      userId,
+      body,
+      subject,
+      stepProspectIds
+    );
     res.send("ok");
   };
 }
 
 EmailQueue.process(async (job, done) => {
-  await Gmail.sendEmail(job.data.endodedEmail, job.data.userId);
+  console.log("Job found!");
+  const sentMail = await Gmail.sendEmail(job.data.endodedEmail, job.data.token);
+  const threadId = sentMail.data.threadId;
+  const StepProspect = await db.StepProspect.findOne({
+    where: { stepId: job.data.stepProspectId },
+  });
+  StepProspect.contacted = true;
+  await StepProspect.save();
+  const CampaignProspect = await db.CampaignProspect.findOne({
+    where: { id: StepProspect.campaignProspectId },
+  });
+  CampaignProspect.threadId = threadId;
+  CampaignProspect.save();
+  const io = Socket.getInstance();
+  const obj = Cache.getSocketObj(job.data.userId);
+  console.log("Cache Obj", obj);
+  obj.sent += 1;
+  Cache.setSocketObj(job.data.userId, obj);
+  io.to(job.data.userId).emit("update", true);
+  io.to(job.data.userId).emit("send email", obj);
   done();
 });
 
-EmailQueue.on("completed", (job, result) => {});
+EmailQueue.on("error", (job, result) => {
+  job.remove();
+});
+
+EmailQueue.on("failed", (job, result) => {
+  job.remove();
+});
+
+EmailQueue.on("completed", (job, result) => {
+  job.remove();
+});
 
 module.exports = Gmail;
