611fde560f0a0cd33eb227a2d484759129ad1511
shah62 <shah62@users.noreply.github.com>
2020-06-11 19:43:08 -0400

    updated req obj to include a socket room

--- server/src/middleware/require-auth.js
+++ server/src/middleware/require-auth.js
@@ -4,7 +4,7 @@
  * If there is no currentUser assigned this middleware would throw a new ForbiddenPathError to the user
  * Simply add the middleware to route and it would authenticate if a user is signed in or not
  */
-const NotLoggedInError = require('../errors/no-logged-in-user')
+const NotLoggedInError = require("../errors/no-logged-in-user");
 const ForbiddenPathError = require("../errors/forbidden-path-error"); //Throws new Forbidden path error
 const jwt = require("jsonwebtoken");
 
@@ -18,7 +18,12 @@ const requireAuth = (req, res, next) => {
   try {
     // Try to check if the jwt has been tampered with or not
     // If not tampered with then assign currentUser property to request or 'req' object
+    if (!req.session.jwt) {
+      throw new ForbiddenPathError();
+    }
     const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY);
+    const socket = req.app.get("socket");
+    socket.join(payload.id);
     req.currentUser = payload;
   } catch (err) {
     // If there is any errors validating the JWT log them to the console
