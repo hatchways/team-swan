e1cba9d74e8149c3a77a069fb06ab3a1a6ca6196
shah62 <shah62@users.noreply.github.com>
2020-06-11 19:34:56 -0400

    Added sending email feature to each step

--- client/src/pages/campaign/Step.js
+++ client/src/pages/campaign/Step.js
@@ -11,6 +11,8 @@ import {
 import { Mail as MailIcon, MoreVert as MoreVertIcon } from "@material-ui/icons";
 import { toSentenceCase } from "js-convert-case";
 import { campaign } from "constants/routes";
+import Gmail from "pages/gmailAuth/Gmail";
+import axios from "axios";
 
 const useStyles = makeStyles((theme) => ({
   container: {
@@ -48,6 +50,8 @@ const Step = ({
   stepId,
   campaignId,
   moveProspectsToStep,
+  hasGmailAuthorized,
+  openGmailModal,
 }) => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [menuAnchor, setMenuAnchor] = useState(null);
@@ -77,75 +81,100 @@ const Step = ({
     setMenuAnchor(e.currentTarget);
   };
 
+  const sendEmail = () => {
+    axios
+      .post("/gmail/sendEmails", {
+        id: stepId,
+      })
+      .then((response) => {})
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
-    <Grid className={container} item container component={Paper}>
-      <Grid
-        item
-        container
-        className={stepInfoContainer}
-        xs={true}
-        alignItems="center"
-        spacing={5}
-      >
-        <Grid item>
-          <MailIcon color="primary" />
-        </Grid>
-        <Grid item>
-          <Typography variant="h4">{order}</Typography>
-        </Grid>
-        <Grid item>
-          <Typography variant="h5">{subject}</Typography>
-          <Typography className={owner} variant="subtitle2">
-            By {userName}
-          </Typography>
-        </Grid>
-      </Grid>
-      {data.map(({ label, value }) => (
+    <>
+      <Grid className={container} item container component={Paper}>
         <Grid
-          className={dataSummaryContainer}
-          key={label}
           item
           container
-          direction="column"
-          alignContent="center"
+          className={stepInfoContainer}
+          xs={true}
+          alignItems="center"
+          spacing={5}
         >
-          <Typography className={dataLabel} align="center">
-            {label}
-          </Typography>
-          <Typography
-            className={dataNumber}
-            color="primary"
-            align="center"
-            variant="h5"
-          >
-            {value || 0}
-          </Typography>
+          <Grid item>
+            <MailIcon color="primary" />
+          </Grid>
+          <Grid item>
+            <Typography variant="h4">{order}</Typography>
+          </Grid>
+          <Grid item>
+            <Typography variant="h5">{subject}</Typography>
+            <Typography className={owner} variant="subtitle2">
+              By {userName}
+            </Typography>
+          </Grid>
         </Grid>
-      ))}
-      <Grid
-        className={optionsButton}
-        item
-        container
-        justify="center"
-        alignItems="center"
-      >
-        <IconButton onClick={clickOptionsMenuHandler}>
-          <MoreVertIcon />
-        </IconButton>
-
-        <Menu
-          elevation={3}
-          anchorEl={menuAnchor}
-          open={isMenuOpen}
-          onClose={() => setIsMenuOpen(false)}
+        {data.map(({ label, value }) => (
+          <Grid
+            className={dataSummaryContainer}
+            key={label}
+            item
+            container
+            direction="column"
+            alignContent="center"
+          >
+            <Typography className={dataLabel} align="center">
+              {label}
+            </Typography>
+            <Typography
+              className={dataNumber}
+              color="primary"
+              align="center"
+              variant="h5"
+            >
+              {value || 0}
+            </Typography>
+          </Grid>
+        ))}
+        <Grid
+          className={optionsButton}
+          item
+          container
+          justify="center"
+          alignItems="center"
         >
-          <MenuItem onClick={clickMoveProspectMenuHandler}>
-            Move Prospects
-          </MenuItem>
-          <MenuItem onClick={clickEditMenuHandler}>Edit</MenuItem>
-        </Menu>
+          <IconButton onClick={clickOptionsMenuHandler}>
+            <MoreVertIcon />
+          </IconButton>
+
+          <Menu
+            elevation={3}
+            anchorEl={menuAnchor}
+            open={isMenuOpen}
+            onClose={() => setIsMenuOpen(false)}
+          >
+            <MenuItem
+              // disabled={data[0].value === data[2].value}
+              onClick={() => {
+                if (!hasGmailAuthorized) {
+                  openGmailModal();
+                } else {
+                  sendEmail();
+                }
+              }}
+            >
+              Send Email
+            </MenuItem>
+            <MenuItem onClick={clickMoveProspectMenuHandler}>
+              Move Prospects
+            </MenuItem>
+            <MenuItem onClick={clickEditMenuHandler}>Edit</MenuItem>
+          </Menu>
+        </Grid>
       </Grid>
-    </Grid>
+    </>
   );
 };
 
